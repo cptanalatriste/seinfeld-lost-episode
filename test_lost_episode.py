34f689145efd71fee6928fdb3021e122e4b4e0d6
Carlos G. Gavidia <cgavidiac@gmail.com>
2020-04-23 18:53:22 +0100

    First checkpoint: Pre-processed data is stored.

--- test_lost_episode.py
+++ test_lost_episode.py
@@ -1,4 +1,6 @@
 import unittest
+
+from helper import preprocess_and_save_data
 from lost_episode import create_int_to_word_map, create_word_to_int_map, create_punctuation_map
 from problem_unittests import test_create_lookup_tables, test_tokenize
 
@@ -6,11 +8,16 @@ from problem_unittests import test_create_lookup_tables, test_tokenize
 class TestLostEpisode(unittest.TestCase):
 
     def test_lookup_tables(self):
-        test_create_lookup_tables(create_lookup_tables=input_function)
+        test_create_lookup_tables(create_lookup_tables=create_maps)
 
     def test_create_punctuation_map(self):
         test_tokenize(token_lookup=create_punctuation_map)
 
+    def test_preprocess_and_save(self):
+        preprocess_and_save_data(dataset_path="data/Seinfeld_Scripts.txt",
+                                 token_lookup=create_punctuation_map,
+                                 create_lookup_tables=create_maps)
 
-def input_function(text):
-    return create_int_to_word_map(text), create_word_to_int_map(text)
+
+def create_maps(text):
+    return create_word_to_int_map(text), create_int_to_word_map(text)
